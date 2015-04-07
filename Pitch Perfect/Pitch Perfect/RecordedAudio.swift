054df7e26f4cd0c28050bc9484f08e8e62884bbe
Jerozan Jeyendrarasa <jerozan.jeyen@gmail.com>
2015-04-07 23:22:24 +0200

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Jerozan Jeyendrarasa on 4/7/15.
+//  Copyright (c) 2015 jerojeyen. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
